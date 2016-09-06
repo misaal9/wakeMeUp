8a72a600e768b04ba885900d87f30091db797f50
Misaal <misaal9@gmail.com>
2016-09-06 14:01:24 +0530

    Removing gh-pages folder

--- gh-pages/app/widgets/jumbotron/viewmodel.js
+++
@@ -1,9 +0,0 @@
-define(function(require){
-    'use strict';
-    
-    var JumbotronWidget = function JumbotronWidget(settings) {
-        this.settings = settings;
-    };
-
-    return JumbotronWidget;
-});
