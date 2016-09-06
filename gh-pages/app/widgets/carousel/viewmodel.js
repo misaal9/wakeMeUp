5fe68298ee58094fb153f06f9699350a911cf6b4
Misaal <misaal9@gmail.com>
2016-09-06 12:38:42 +0530

    Update branch w/ latest changes.

--- gh-pages/app/widgets/carousel/viewmodel.js
+++
@@ -1,9 +0,0 @@
-define(function(require){
-    'use strict';
-    
-    var CarouselWidget = function CarouselWidget(settings) {
-        this.settings = settings;
-    };
-
-    return CarouselWidget;
-});
